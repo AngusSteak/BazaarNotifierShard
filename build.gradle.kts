0ce9ace6daf5e764adee39c3047556855883ecde
AngusSteak <91897161+AngusSteak@users.noreply.github.com>
2025-07-11 21:06:14 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     idea
     java
-    id("gg.essential.loom") version "0.10.0.+"
+    id("gg.essential.loom") version "0.10.0"
     id("dev.architectury.architectury-pack200") version "0.1.3"
     id("de.undercouch.download").version("5.3.0")
     id("com.github.johnrengelman.shadow") version "7.1.2"
