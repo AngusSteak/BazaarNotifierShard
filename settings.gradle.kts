eb0c922dacfe0ebbd9e382e461d543389eec2e90
AngusSteak <91897161+AngusSteak@users.noreply.github.com>
2025-07-11 22:28:54 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,20 @@
 pluginManagement {
     repositories {
         mavenCentral()
+        mavenLocal()
         gradlePluginPortal()
-        maven("https://maven.essential.gg/releases")
+        maven("https://repo.essential.gg/repository/maven-public")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://maven.architectury.dev/")
         maven("https://maven.fabricmc.net")
         maven("https://maven.minecraftforge.net/")
         maven("https://repo.spongepowered.org/maven/")
-        maven("https://repo.sk1er.club/repository/maven-releases/")
-        maven("https://repo.essential.gg/repository/maven-public")
+        
     }
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
-                "gg.essential.loom" -> useModule("gg.essential:loom:${requested.version}")
+                "gg.essential.loom" -> useModule("gg.essential:architectury-loom:${requested.version}")
             }
         }
     }
